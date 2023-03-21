57e7c75c3f8f972dfbece28f3e52fb4e538911dd
Carlos Trujillo <krloslao90@gmail.com>
2023-03-20 23:25:27 -0400

    fixing errata

--- src/pages/pp.tsx
+++ src/pages/pp.tsx
@@ -7,7 +7,7 @@ import TitleSection from 'components/ui/TitleSection';
 
 const PP: React.FC = () => (
   <Layout>
-    <SEO title="404 PPr" />
+    <SEO title="404 PP" />
     <Container section>
       <TitleSection title="404" subtitle="Oops! Looks like we compressed this page a little too much..." center />
       <p className="mt-4 text-center w-full">We have a Weismann Score of 5.2 after all 😏</p>
